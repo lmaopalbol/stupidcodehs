0cd0579a8b228bd9d1e0a3000e4cac4a3507371a
lmaopalbol <97364497+lmaopalbol@users.noreply.github.com>
2023-11-02 09:57:58 -0400

    Update amongus.js

--- amongus.js
+++ amongus.js
@@ -1,1 +1,58 @@
+var requiredClicks = 100 + 14
+var movement = getWidth()/requiredClicks
+var rect = new Rectangle(50,50)
+var clicks = 0;
+var printed = false;
+var time = 0;
+var startTime;
 
+function start(){
+    makeBlock();
+    setTimer(winCheck, 40)
+}
+
+function makeBlock(){
+    var road = new Rectangle(getWidth(), getHeight()/3)
+    road.setColor(Color.gray)
+    road.setPosition(0,getHeight()/3)
+    add(road)
+    
+    let yellow = new Rectangle(road.getWidth()/8, road.getHeight()/8)
+    yellow.setColor(Color.yellow)
+    yellow.setPosition(getWidth()/2, getHeight()/2 - yellow.getHeight()/2)
+    add(yellow)
+    
+    rect.setColor(Color.black)
+    rect.setPosition(0, (getHeight()/2)+25)
+    add(rect)
+}
+
+window.addEventListener('click', function(q){
+    if(clicks==0){
+        const start = Date.now();
+        startTime = start
+    }
+
+    if(clicks!=100){
+        clicks++
+        rect.move(movement, 0)
+    }
+});
+
+function winCheck(){
+    if(clicks == 100 && printed!=true){
+        total();
+        var text = new Text("You win! CPS: " + Math.round(100/time))
+        text.setPosition(getWidth()/2 - (text.getWidth()/2), getHeight()/2 + (text.getHeight()/2))
+        add(text)
+        printed = true
+    }
+}
+
+function total(){
+    var millis = Date.now() - startTime;
+
+    var tot = millis/1000
+
+    time = tot    
+}
